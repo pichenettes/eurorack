2eba1bf7b823fa8914cbe5a7985c83a9acaa5dd4
Olivier Gillet <ol.gillet@gmail.com>
2014-05-14 19:21:54 +0200

    Upgraded to latest version of private code

--- yarns/part.cc
+++ yarns/part.cc
@@ -649,6 +649,7 @@ int16_t Part::Tune(int16_t midi_note) {
     pitch += custom_pitch_table_[pitch_class];
   } else if (voicing_.tuning_system > TUNING_SYSTEM_JUST_INTONATION) {
     note -= voicing_.tuning_root;
+    pitch_class = (note + 240) % 12;
     pitch += lookup_table_table[LUT_SCALE_PYTHAGOREAN + \
         voicing_.tuning_system - TUNING_SYSTEM_PYTHAGOREAN][pitch_class];
   }
