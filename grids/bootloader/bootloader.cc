3e1f07e4e4b85387d6d750b9064e51d86fdb92c4
Olivier Gillet <ol.gillet@gmail.com>
2014-11-14 11:14:12 +0100

    Grids: audio bootloader

--- grids/bootloader/bootloader.cc
+++ grids/bootloader/bootloader.cc
@@ -26,17 +26,22 @@
 #include "avrlib/serial.h"
 #include "avrlib/watchdog_timer.h"
 
+#include "avr_audio_bootloader/crc32.h"
+#include "avr_audio_bootloader/fsk/decoder.h"
+
 #include "grids/hardware_config.h"
 
 using namespace avrlib;
 using namespace grids;
+using namespace avr_audio_bootloader;
 
 MidiInput midi;
 Inputs inputs;
 Leds leds;
+Decoder decoder;
 
 uint16_t page = 0;
-uint8_t rx_buffer[SPM_PAGESIZE + 1];
+uint8_t rx_buffer[SPM_PAGESIZE + 4];
 
 void (*main_entry_point)(void) = 0x0000;
 
@@ -84,6 +89,8 @@ void FlashLedsError() {
   }
 }
 
+// MIDI loader -----------------------------------------------------------------
+
 static const uint8_t sysex_header[] = {
   0xf0,  // <SysEx>
   0x00, 0x21, 0x02,  // Mutable instruments manufacturer id.
@@ -96,7 +103,7 @@ enum SysExReceptionState {
   READING_DATA = 2,
 };
 
-inline void MidiLoop() {
+inline void LoaderLoop() {
   uint8_t byte;
   uint16_t bytes_read = 0;
   uint16_t rx_buffer_index;
@@ -107,77 +114,124 @@ inline void MidiLoop() {
   uint8_t page_byte = 0;
 
   midi.Init();
+  decoder.Init();
+  decoder.Sync();
+  decoder.set_packet_destination(rx_buffer);
   page = 0;
-  
+
+  TCCR2A = 0;
+  TCCR2B = 2;
+
   while (1) {
     leds.Write(LED_CLOCK | (LED_BD >> ((page_byte + 3) & 0x3)));
-    byte = midi.Read();
-    // In case we see a realtime message in the stream, safely ignore it.
-    if (byte > 0xf0 && byte != 0xf7) {
-      continue;
-    }
-    switch (state) {
-      case MATCHING_HEADER:
-        if (byte == sysex_header[bytes_read]) {
-          ++bytes_read;
-          if (bytes_read == sizeof(sysex_header)) {
-            bytes_read = 0;
-            state = READING_COMMAND;
-          }
-        } else {
-          bytes_read = 0;
-        }
-        break;
 
-      case READING_COMMAND:
-        if (byte < 0x80) {
-          sysex_commands[bytes_read++] = byte;
-          if (bytes_read == 2) {
-            bytes_read = 0;
-            rx_buffer_index = 0;
-            checksum = 0;
-            state = READING_DATA;
-          }
-        } else {
-          state = MATCHING_HEADER;
-          status = 0;
-          bytes_read = 0;
-        }
-        break;
+    // Sample the clock input at 20kHz and feed to the FSK decoder.
+    if (TCNT2 >= (F_CPU / 8 / 40000 - 1)) {
+      TCNT2 = 0;
+      
+      switch (decoder.PushSample(inputs.Read() & INPUT_CLOCK)) {
+        case DECODER_STATE_ERROR_SYNC:
+          FlashLedsError();
+          decoder.Sync();
+          break;
 
-      case READING_DATA:
-        if (byte < 0x80) {
-          if (bytes_read & 1) {
-            rx_buffer[rx_buffer_index] |= byte & 0xf;
-            if (rx_buffer_index < SPM_PAGESIZE) {
-              checksum += rx_buffer[rx_buffer_index];
+        case DECODER_STATE_END_OF_TRANSMISSION:
+          return;
+          break;
+
+        case DECODER_STATE_PACKET_RECEIVED:
+          {
+            uint32_t crc = crc32(0, rx_buffer, SPM_PAGESIZE);
+            uint32_t expected_crc = \
+                (static_cast<uint32_t>(rx_buffer[SPM_PAGESIZE + 0]) << 24) | \
+                (static_cast<uint32_t>(rx_buffer[SPM_PAGESIZE + 1]) << 16) | \
+                (static_cast<uint32_t>(rx_buffer[SPM_PAGESIZE + 2]) <<  8) | \
+                (static_cast<uint32_t>(rx_buffer[SPM_PAGESIZE + 3]) <<  0);
+            if (crc == expected_crc) {
+              WriteBufferToFlash();
+              page += SPM_PAGESIZE;
+              ++page_byte;
+            } else {
+              FlashLedsError();
             }
-            ++rx_buffer_index;
-          } else {
-            rx_buffer[rx_buffer_index] = (byte << 4);
           }
-          ++bytes_read;
-        } else if (byte == 0xf7) {
-          if (sysex_commands[0] == 0x7f &&
-              sysex_commands[1] == 0x00 &&
-              bytes_read == 0) {
-            // Reset.
-            return;
-          } else if (rx_buffer_index == SPM_PAGESIZE + 1 &&
-                     sysex_commands[0] == 0x7e &&
-                     sysex_commands[1] == 0x00 &&
-                     rx_buffer[rx_buffer_index - 1] == checksum) {
-            // Block write.
-            WriteBufferToFlash();
-            page += SPM_PAGESIZE;
-            ++page_byte;
+          decoder.Sync();
+          break;
+
+        default:
+          break;
+      }
+    }
+    
+    // Poll the MIDI input.
+    if (midi.readable()) {
+      byte = midi.ImmediateRead();
+      if (byte > 0xf0 && byte != 0xf7) {
+        continue;
+      }
+      switch (state) {
+        case MATCHING_HEADER:
+          if (byte == sysex_header[bytes_read]) {
+            ++bytes_read;
+            if (bytes_read == sizeof(sysex_header)) {
+              bytes_read = 0;
+              state = READING_COMMAND;
+            }
           } else {
-            FlashLedsError();
+            bytes_read = 0;
           }
-          state = MATCHING_HEADER;
-          bytes_read = 0;
-        }
-        break;
+          break;
+
+        case READING_COMMAND:
+          if (byte < 0x80) {
+            sysex_commands[bytes_read++] = byte;
+            if (bytes_read == 2) {
+              bytes_read = 0;
+              rx_buffer_index = 0;
+              checksum = 0;
+              state = READING_DATA;
+            }
+          } else {
+            state = MATCHING_HEADER;
+            status = 0;
+            bytes_read = 0;
+          }
+          break;
+
+        case READING_DATA:
+          if (byte < 0x80) {
+            if (bytes_read & 1) {
+              rx_buffer[rx_buffer_index] |= byte & 0xf;
+              if (rx_buffer_index < SPM_PAGESIZE) {
+                checksum += rx_buffer[rx_buffer_index];
+              }
+              ++rx_buffer_index;
+            } else {
+              rx_buffer[rx_buffer_index] = (byte << 4);
+            }
+            ++bytes_read;
+          } else if (byte == 0xf7) {
+            if (sysex_commands[0] == 0x7f &&
+                sysex_commands[1] == 0x00 &&
+                bytes_read == 0) {
+              // Reset.
+              return;
+            } else if (rx_buffer_index == SPM_PAGESIZE + 1 &&
+                       sysex_commands[0] == 0x7e &&
+                       sysex_commands[1] == 0x00 &&
+                       rx_buffer[rx_buffer_index - 1] == checksum) {
+              // Block write.
+              WriteBufferToFlash();
+              page += SPM_PAGESIZE;
+              ++page_byte;
+            } else {
+              FlashLedsError();
+            }
+            state = MATCHING_HEADER;
+            bytes_read = 0;
+          }
+          break;
+      }
     }
   }
 }
@@ -188,7 +242,7 @@ int main(void) {
   _delay_ms(40);
   if (!(inputs.Read() & INPUT_SW_RESET)) {
     FlashLedsOk();
-    MidiLoop();
+    LoaderLoop();
     FlashLedsOk();
     FlashLedsOk();
   }
