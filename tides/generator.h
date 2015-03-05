f2ffe52d8f149a02a6f653d58741d11ee3d273d9
Olivier Gillet <ol.gillet@gmail.com>
2015-03-05 10:49:56 +0100

    Tides: added missing declaration

--- tides/generator.h
+++ tides/generator.h
@@ -236,6 +236,7 @@ class Generator {
   uint32_t local_osc_phase_;
   uint32_t local_osc_phase_increment_;
   uint32_t target_phase_increment_;
+  uint32_t eor_counter_;
   
   stmlib::PatternPredictor<32, 8> pattern_predictor_;
   
